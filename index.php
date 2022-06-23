a3546533ca7362df0f3544342c03109c69ca80da
Jinmoris PARK <jinmoris@naver.com>
2022-06-24 04:06:56 +0900

    test

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
-echo "welcome";
+    $navi = "welcome";
+    $main = "main page";
+    $footer = "footer page";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <navi>
+        <?php echo $navi; ?>
+</navi>
+<main>
+    <?php echo $main; ?>
+</main>
+<footer>
+<?php echo $footer; ?>
+</footer>
+    
+</body>
+</html>
